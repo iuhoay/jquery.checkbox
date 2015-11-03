3c12a135b5b1b75b07102d38851e48742c1e81c8
iuhoay <iuhoay@gmail.com>
2015-11-03 16:17:21 +0800

    add callback function

--- jquery.checkbox.js
+++ jquery.checkbox.js
@@ -1,22 +1,32 @@
 /**
-* jquery.checkbox.js v0.0.1
+* jquery.checkbox.js v0.0.2
 * https://github.com/iuhoay/jquery.checkbox
 */
 (function($){
-  $.fn.checkbox = function() {
+  $.fn.checkbox = function(callback) {
     var $el = $(this),
-        children_select = $el.data("children");
+        children_select = $el.data("children"),
+        global_this = this;
 
     if (!children_select) return false;
 
     $el.on("change", function() {
       $(children_select).prop("checked", $el.prop("checked"));
+      children_change();
     })
 
     $(children_select).on("change", function() {
-      var $children = $(children_select);
-      $el.prop("checked", $children.length == $(children_select + ":checked").length);
+      children_change();
     })
+
+    var children_change = function() {
+      var $children = $(children_select);
+      $el.prop("checked", $children.length == $children.filter(":checked").length)
+
+      if (typeof callback === 'function') {
+        callback(global_this.values());
+      }
+    }
 
     this.values = function() {
       var result = [];
